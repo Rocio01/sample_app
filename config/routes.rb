9cf02a2a275ea9ac9dde76a423146e2659e295c7
exit <zrmartinezg@gmail.com>
2021-04-09 16:14:05 -0500

    Add about, contact pages, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'  
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+
 end
